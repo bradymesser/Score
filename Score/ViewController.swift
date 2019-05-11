b77b19cf8df2c82cc49e56f992b62eee3931ab5b
William B Messer <wmesser@clemson.edu>
2019-05-11 10:44:47 -0400

    Init

---
+++ Score/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Score
+//
+//  Created by Brady on 5/11/19.
+//  Copyright © 2019 William Messer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
