b2b9c1ad336f1a2f3aa56868bfc763227f7d5bee
William B Messer <wmesser@clemson.edu>
2019-05-12 11:33:22 -0400

    Add search on the list of games

--- Score/Views/VideoViewController.swift
+++ Score/Views/VideoViewController.swift
@@ -4,22 +4,34 @@ import UIKit
 //Displays the list of games, tapping on a game takes you to a view that shows all the available videos for the game
 class VideoViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
     var videos: [SoccerVideoModel] = []
+    var searchActive = false
+    var filtered: [SoccerVideoModel] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
         tableView.delegate = self
+        searchBar.delegate = self
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if searchActive {
+            return filtered.count
+        }
         return videos.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         if let cell = cell as? CustomCell {
-            cell.textLabel?.text = videos[indexPath.row].title
+            if searchActive {
+                cell.textLabel?.text = filtered[indexPath.row].title
+
+            } else {
+                cell.textLabel?.text = videos[indexPath.row].title
+            }
         }
         return cell
     }
@@ -28,7 +40,11 @@ class VideoViewController: UIViewController, UITableViewDataSource, UITableViewD
         tableView.deselectRow(at: indexPath, animated: false)
         let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
         let destination = storyboard.instantiateViewController(withIdentifier: "GameView") as! GameViewController
-        destination.game = videos[indexPath.row]
+        if searchActive {
+            destination.game = filtered[indexPath.row]
+        } else {
+            destination.game = videos[indexPath.row]
+        }
         navigationController?.pushViewController(destination, animated: true)
     }
 }
@@ -37,4 +53,39 @@ class VideoViewController: UIViewController, UITableViewDataSource, UITableViewD
 class CustomCell: UITableViewCell {
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var thumbnail: UIImageView!
+}
+
+extension VideoViewController: UISearchBarDelegate {
+    func searchBarTextDidBeginEditing(_: UISearchBar) {
+        searchActive = true
+    }
+    
+    func searchBarTextDidEndEditing(_: UISearchBar) {
+        searchActive = false
+    }
+    
+    func searchBarCancelButtonClicked(_: UISearchBar) {
+        searchActive = false
+        filtered.removeAll(keepingCapacity: false)
+        tableView.reloadData()
+    }
+    
+    func searchBarSearchButtonClicked(_: UISearchBar) {
+        searchActive = false
+    }
+    
+    func searchBar(_: UISearchBar, textDidChange searchText: String) {
+        filtered.removeAll(keepingCapacity: false)
+        for game in videos {
+            if game.title.lowercased().contains(searchText.lowercased()) {
+                filtered.append(game)
+            }
+        }
+        if filtered.count == 0 {
+            searchActive = false
+        } else {
+            searchActive = true
+        }
+        tableView.reloadData()
+    }
 }
